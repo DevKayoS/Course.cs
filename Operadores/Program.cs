8f2192bc53988060087e0737f02b9f9e90ff6604
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-07-04 14:25:29 -0300

    feat: aprendendo sobre operações aritiméticas

--- Operadores/Program.cs
+++ Operadores/Program.cs
@@ -29,11 +29,39 @@ namespace Operators
             int c = 10;
             c--;
             Console.WriteLine(c);
-
+ 
             int d = 10;
             int e = ++d;
             Console.WriteLine(d);
             Console.WriteLine(e);
+
+            Console.WriteLine("------------------------------");
+            // operadores aritméticos
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 9 % 3;
+
+            double n5 = (double) 10 / 8;
+
+            //entenda x sendo a, y sendo b e z sendo c
+            double x = 1.0, y = -3.0, z = -4.0;
+
+            double delta = (Math.Pow(y, 2.0)) - (4.0 * x * z);
+
+            double x1 = (-y + Math.Sqrt(delta) ) / (2.0 * x);
+            double x2 = (-y - Math.Sqrt(delta)) / (2.0 * x);
+
+
+            Console.WriteLine(delta);
+
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n5);
         }
     }
 }
