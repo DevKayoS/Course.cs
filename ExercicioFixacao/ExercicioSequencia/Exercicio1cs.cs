a578eb8df9826efa251d97b15d5d1a37882d8c69
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-07-13 12:58:34 -0300

    feat: aprendendo sobre operadores  logicos e comparativos

---
+++ ExercicioFixacao/ExercicioSequencia/Exercicio1cs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+            Console.WriteLine("Insira o valor do raio:");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //fazendo o calculo da area
+            area = 3.14159 * (Math.Pow(raio, 2.0));
+            //imprimindo resultado
+            Console.WriteLine($"A={area.ToString(CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Logico/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Logicos
+{
+    class Program
+    {
+         static void Main()
+        {
+            bool c1 = 2 > 3 || 4 != 5;
+            bool c2 = !(2 > 3) && 4 != 5;
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+            Console.WriteLine("--------------------");
+            bool c3 = 10 < 5;
+            bool c4 = c1 || c2 && c3;
+            Console.WriteLine(c4);
+        }
+    }
+}
