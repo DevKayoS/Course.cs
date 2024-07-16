aaecd7db5890819d748a8703280f9eda67d012bf
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-07-15 22:55:59 -0300

    feat: aprendendo sobre funcoes no csharp

---
+++ Functions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite tres numeros: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            int resultado = BiggestNumber(n1, n2, n3);
+
+            Console.WriteLine($"O maior numero: {resultado}");
+        }
+
+        static int BiggestNumber(int n1, int n2, int n3)
+        {
+            int biggerNumber;
+
+            if (n1 > n2 && n1 > n3)
+            {
+                biggerNumber = n1;
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                biggerNumber = n2;
+
+            }
+            else if (n3 > n2 && n3 > n1)
+            {
+                biggerNumber = n3;
+            }
+
+            return biggerNumber;
+
+        }
+    }
+}
